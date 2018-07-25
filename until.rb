e3e137a84abac4d104ef9d95ac404452ffef3aa5
Grace Park <parkgra3@gmail.com>
2018-07-25 14:30:09 +0000

    Done.

---
+++ until.rb
@@ -0,0 +1,17 @@
+def hungry_gp
+  mango = 0 
+  until mango ==3
+  puts "Give Me MY Mango!"
+  mango += log10end
+  end
+end
+
+puts hungry_gp
+
+character_list = {
+  "Monica" => "Neat Freak",
+  "Joey
+  
+}
+
+#Key is name, value is characteristic eg Neat Freak
---
+++ while.rb
@@ -0,0 +1,9 @@
+def hungry_mn
+  kitkat = 0 
+  while kitkat <30
+  puts "Give me another Kitkat!"
+  kitkat -= 1 
+end 
+end
+hungry_mn
+
